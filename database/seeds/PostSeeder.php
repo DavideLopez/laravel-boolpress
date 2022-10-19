837e95e785b73959f716fc7d2277ca2506f924e5
DavideLopez <davidelopez7@gmail.com>
2022-10-19 16:07:55 +0200

    add ruote - post controller e index

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $post = new Post();
+            $post->title = $faker->words(rand(5, 10), true);
+            $post->content = $faker->paragraphs(rand(10, 20), true);
+            $post->slug = str_replace(' ', '-', $post->title);
+
+            $post->save();
+        }
+    }
+}
