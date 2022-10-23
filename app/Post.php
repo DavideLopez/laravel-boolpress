18f8cee483093d61e9c38f70f65947d72a23059e
DavideLopez <davidelopez7@gmail.com>
2022-10-23 13:26:09 +0200

    categoria show

--- app/Post.php
+++ app/Post.php
@@ -4,11 +4,34 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Post extends Model
 {
-    protected $fillable = [
+    protected $fillable=[
         'title',
         'content',
-        'slug'
+        'slug',
+        'category_id'
     ];
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+
+    static public function getUniqueSlugFromTitle($title){
+
+        $slug_base = Str::slug($title);
+        $slug=$slug_base;
+        //controllare che sia unico
+        //usando una query sql
+        $post_esistente = Post::where('slug', $slug_base)->first();
+        $counter = 1;
+        //se il post esiste cambio slug e rifaccio la ricerca
+        while ($post_esistente) {
+            $slug = $slug_base .'-'.$counter;
+            $post_esistente = Post::where('slug', $slug)->first();
+            $counter++;
+        }
+        return $slug;
+    }
 }
