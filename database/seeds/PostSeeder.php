00cd4b7e0a73f2f0e1b9248a9f3439873a364a73
DavideLopez <davidelopez7@gmail.com>
2022-10-24 17:14:11 +0200

    fix seeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use App\Category;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -9,12 +12,17 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         for ($i = 0; $i < 50; $i++) {
+
+            $category_ids = Category::All()->pluck('id');
+
             $post = new Post();
+
             $post->title = $faker->words(rand(5, 10), true);
             $post->content = $faker->paragraphs(rand(10, 20), true);
+            $post->category_id = $faker->optional()->randomElement($category_ids);
             $post->slug = str_replace(' ', '-', $post->title);
 
             $post->save();
